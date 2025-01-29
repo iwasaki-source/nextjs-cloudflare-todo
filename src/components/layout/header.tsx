197040d5a2336471e01d172639800e96168d1bc2
iwasaki <poettype73@gmail.com>
2025-01-29 12:36:37 +0900

    header文章修正

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="flex justify-between items-center bg-gray-950 px-10 py-4">
       <Link href='/' className="text-white text-2xl">Todo App</Link>
-      <Link href="/create" className="inline-block bg-amber-400 px-6 py-2 rounded-md text-center">作成</Link>
+      <Link href="/create" className="inline-block bg-amber-400 px-6 py-2 rounded-md text-center">作成ページへ</Link>
     </header>
   )
 }
